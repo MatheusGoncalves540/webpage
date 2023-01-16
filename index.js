8ef8858fc1826642ef66df37aed7535003811d05
Matheus <matheus.goncalves@ammovarejo.com.br>
2023-01-16 14:08:14 -0300

    adições de estilo e funcionalidade

--- index.js
+++ index.js
@@ -14,14 +14,18 @@ function click()
 
 function ContarSegundos(){
     segundos += 1;
+    
     atualizartempo();
+    
 } setInterval(ContarSegundos, 1000);
 
 
 function atualizartempo(){
     document.getElementById("tempo").innerHTML ="Já passou " + segundos +" segundos...";
+    document.querySelector(".rotate_minuto").style.transform = 'rotate(' + (segundos * 0.1) + 'deg)';
+    document.querySelector(".rotate_hora").style.transform = 'rotate(' + (segundos * 0.001666) + 'deg)';
+    document.querySelector(".rotate_segundo").style.transform = 'rotate(' + (segundos * 6) + 'deg)';
 }
-//Funções
 
 atualizartempo();
 
